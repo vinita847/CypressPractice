86c78ac0c65c2c50900eb863c49028c7a954b3e7
vinita847 <samantvinita25@gmail.com>
2023-06-01 16:18:25 +1200

    chnages in user flow test

--- cypress/e2e/userFlow.cy.ts
+++ cypress/e2e/userFlow.cy.ts
@@ -2,13 +2,13 @@ describe("test for user flow", () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000');    
       })
-    it.only("a user flow from home page to course lesson", () => {
+    it("a user flow from home page to course lesson", () => {
         cy.getByData("course-0").find("a").contains("Get started").click();
         cy.url().should('include', "/testing-your-first-application");
        
         cy.getByData("next-lesson-button").should("exist").click();
-        cy.wait(5000);
-        cy.get('h1#app-install--overview').should('be.visible');
+        //cy.wait(5000);
+        cy.get('h1#app-install--overview', {timeout: 5000}).should('be.visible');
 
 
         cy.url().should('include', "/app-install-and-overview");
@@ -29,31 +29,37 @@ describe("test for user flow", () => {
 
 
         cy.getByData("next-lesson-button").should("be.visible").click();
+        cy.url().should("include", ":3000");
+        cy.getByData("hero-heading").should("be.visible");
+        
+        for(var i=0; i<3; i++) {
+
+            cy.getByData(`lesson-complete-${i}`).should("exist")
+
+        }     // can i USE forEach here?
 
 
         
     })
 
-    it("a user flow for course Testing Foundation Course and verify user complets all", () => {
-        cy.getByData("course-1").find("a").contains("Get started").click();
-        cy.url().contains("/testing-foundations");
-        cy.getByData("next-lesson-button").should("exist").click();
-        cy.location("pathname").should("equal", "/testing-is-a-mindset");
-        cy.getByData("challenge-answer-0").should("be.visible").check();
-        cy.getByData("next-lesson-button").should("be.visible").click();
-        cy.location("pathname").should("equal", "/knowing-what-to-test");
-        cy.getByData("challenge-answer-0").should("be.visible").check();
-        cy.getByData("next-lesson-button").should("be.visible").click();        
-        cy.location("pathname").should("equal", "/manual-vs-automated-testing");
-        cy.getByData("challenge-answer-0").should("be.visible").check();
-        cy.getByData("next-lesson-button").should("be.visible").contains("Complete Course").click();
-        cy.location("pathname").should("equal", "http://localhost:3000/");
-        //verify that after competing the course, all check boxes are checked.
-        cy.getByData("course-1").find("lesson-complete-0").should("be.visible");
-
-        
+    // it("a user flow for course Testing Foundation Course and verify user complets all", () => {
+    //     cy.getByData("course-1").find("a").contains("Get started").click();
+    //     cy.url().contains("/testing-foundations");
+    //     cy.getByData("next-lesson-button").should("exist").click();
+    //     cy.location("pathname").should("equal", "/testing-is-a-mindset");
+    //     cy.getByData("challenge-answer-0").should("be.visible").check();
+    //     cy.getByData("next-lesson-button").should("be.visible").click();
+    //     cy.location("pathname").should("equal", "/knowing-what-to-test");
+    //     cy.getByData("challenge-answer-0").should("be.visible").check();
+    //     cy.getByData("next-lesson-button").should("be.visible").click();        
+    //     cy.location("pathname").should("equal", "/manual-vs-automated-testing");
+    //     cy.getByData("challenge-answer-0").should("be.visible").check();
+    //     cy.getByData("next-lesson-button").should("be.visible").contains("Complete Course").click();
+    //     cy.location("pathname").should("equal", "http://localhost:3000/");
+    //     //verify that after competing the course, all check boxes are checked.
+    //     cy.getByData("course-1").find("lesson-complete-0").should("be.visible");  
 
 
         
-    })
+    // })
 })
